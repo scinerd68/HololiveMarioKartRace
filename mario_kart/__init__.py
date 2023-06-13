511c7e67240b75a7302520f8bfccc50382329523
Phan Thang <thangphan0168@gmail.com>
2023-06-13 22:32:20 +0700

    Modify database schema andd add some basic routes

--- mario_kart/__init__.py
+++ mario_kart/__init__.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///race.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///race.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+from mario_kart.main.routes import main
+from mario_kart.players.routes import players
+
+app.register_blueprint(main)
+app.register_blueprint(players)
--- mario_kart/models.py
+++ mario_kart/models.py
@@ -2,29 +2,33 @@ from mario_kart import db
 
 
 class Tournament(db.Model):
+    __tablename__ = 'Tournament'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
-    race = db.relationship('RaceDetails', backref='tournament', lazy=True)
+    race = db.relationship('RaceDetails', backref='Tournament', lazy=True)
 
 
 class Player(db.Model):
+    __tablename__ = 'Player'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     current_elo = db.Column(db.Integer, nullable=False, default=1000)
-    race = db.relationship('RaceDetails', backref='player', lazy=True)
+    race = db.relationship('RaceDetails', backref='Player', lazy=True)
 
 
 tournament_player = db.Table('TournamentPlayer',
-    db.Column('tournament_id', db.Integer, db.ForeignKey('tournament.id'), primary_key=True),
-    db.Column('player_id', db.Integer, db.ForeignKey('player.id'), primary_key=True)
+    db.Column('tournament_id', db.Integer, db.ForeignKey('Tournament.id'), primary_key=True),
+    db.Column('player_id', db.Integer, db.ForeignKey('Player.id'), primary_key=True)
 )
 
 
 class RaceDetails(db.Model):
+    __tablename__ = 'RaceDetails'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'), nullable=False)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    tournament_id = db.Column(db.Integer, db.ForeignKey('Tournament.id'), nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey('Player.id'), nullable=False)
     race_position = db.Column(db.Integer, nullable=False)
+    player_old_elo = db.Column(db.Integer, nullable=False)
     elo_change = db.Column(db.Integer, nullable=False)
     disconnected = db.Column(db.Integer, nullable=False)
